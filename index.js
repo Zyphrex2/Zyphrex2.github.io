7733b2762419a6b2b5d05e2fce1e6a027f395d69
IceLuigi7592 <82611154+IceLuigi7592@users.noreply.github.com>
2024-07-19 21:27:23 -0500

    added dropdown, made dict

---
+++ index.js
@@ -0,0 +1,11 @@
+instdict = {
+    "Flutes":[],
+    "Clarinets":[],
+    "Saxophones":[],
+    "Trumpets":[],
+    "F Horns":[],
+    "Trombones":[],
+    "Baritones":[],
+    "Tubas":[]
+}
+console.log()
