85611ee1e5241b3738d852366dd2b1aef03c9180
IceLuigi7592 <82611154+IceLuigi7592@users.noreply.github.com>
2025-01-16 20:35:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*
 vegetarian restaurant website, dine in and carry out, methods and approach
 
-vegetarian sandwich shop, 
+vegetarian sandwich shop, serve sandwiches, appetizers, dessert, pasta.
+hamburger menu, with menu, order/pickup now, about us, help, job application page, account creation system, google maps integration with address?
 */
